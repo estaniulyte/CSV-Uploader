a83b7b58a3b8a01f018c5db212609b4eb01ab03c
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-02-23 12:16:25 +0200

    feat: parse CSV file using Papa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Papa from 'papaparse';
+import { useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [columnArray, setColumn] = useState([]);
+  const [values, setValues] = useState([]);
+
+  const handleFile = (e) => {
+    Papa.parse(e.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function(result) {
+        const columnArray = [];
+        const valuesArray = [];
+
+        result.data.map((d) => {
+          columnArray.push(Object.keys(d));
+          valuesArray.push(Object.values(d));
+        })
+
+        setData(result.data);
+        setColumn(columnArray[0]);
+        setValues(valuesArray);
+      }
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
