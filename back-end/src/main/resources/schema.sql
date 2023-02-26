d9c8e163565c3cbc398f62ce8de23a9534ab01f9
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-02-26 21:26:58 +0200

    delete: delete schema.sql before used for testing

--- back-end/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-create table employee(
-    id int auto_increment,
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    phone varchar(255) NOT NULL
-);
-
-insert into employee(name, email, phone) values ('John Smith', 'john.smith@gmail.com', '86-554-232');
